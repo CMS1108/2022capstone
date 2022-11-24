0c9bb4ab75888b93a441c4ef024d4ef76142e427
Minseon Kim <minsun9856@gmail.com>
2022-11-24 18:30:19 +0900

    불필요한 주석 제거

--- lib/ui/widget/remain_stat_widget.dart
+++ lib/ui/widget/remain_stat_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,41 +9,45 @@ class RemainStat extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     if(ableCnt == 0){
-      return Container(
-        width: MediaQuery.of(context).size.width * 0.95,
-        padding: EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 2),
-            borderRadius: BorderRadius.circular(10.0),
-            color: Colors.red[300]
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 10.0,
+      return Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.95,
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.red[300]
             ),
-            Expanded(
-              flex: 1,
-              child: Image.asset('assets/images/tears.png', width: 20.0),
-            ),
-            SizedBox(
-              width: 22.0,
-            ),
-            Expanded(
-                flex: 4,
-                child: Text(
-                    '현재 주차 가능 구역이 없어요',
-                    style: GoogleFonts.lato(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black)
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Image.asset('assets/images/tears.png', width: 20.0),
+                ),
+                SizedBox(
+                  width: 22.0,
+                ),
+                Expanded(
+                    flex: 4,
+                    child: Text(
+                        '현재 주차 가능 구역이 없어요',
+                        style: GoogleFonts.lato(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black)
+                    )
                 )
-            )
 
-          ],
-        ),
+              ],
+            ),
+          ),
+        ],
       );
     } else if(ableCnt < 10){
       return Container(
@@ -164,11 +169,9 @@ class RemainStat extends StatelessWidget{
                       ),
                     ],
                   )
-
                 ],
               ),
             )
-
           ],
         ),
       );
