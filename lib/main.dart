2571fb4195853a5b0d60ca78dae58cb629c80f94
Minseon Kim <minsun9856@gmail.com>
2022-11-24 18:28:39 +0900

    앱 이름 수정, 가로 모드 회전 막음

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:parkwhere/ui/view/main_page.dart';
 import 'package:parkwhere/viewmodel/parking_lot_model.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
 
@@ -20,9 +21,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp, // 세로 정방향
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: '파킹웨어',
+      title: '주정차 알리미',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
